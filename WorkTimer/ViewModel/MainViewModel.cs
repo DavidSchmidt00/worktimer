6e9f51fdbd63917b9651af37be424cd6a33b5d57
DavidSchmidt00 <davod2211@gmail.com>
2023-07-06 23:36:55 +0200

    #2: UI Grundstruktur

---
+++ WorkTimer/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WorkTimer.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        int time;
+
+        [RelayCommand]
+        void Start()
+        {
+            Time++;
+        }
+    }
+}
