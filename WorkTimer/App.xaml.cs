6b7115fc00daf99c3a93966cefef159a9681c183
DavidSchmidt00 <davod2211@gmail.com>
2023-07-27 17:04:51 +0200

    #4: Implement worktime repository

--- WorkTimer/App.xaml.cs
+++ WorkTimer/App.xaml.cs
@@ -1,11 +1,16 @@
-﻿namespace WorkTimer;
+﻿using WorkTimer.Database;
+
+namespace WorkTimer;
 
 public partial class App : Application
 {
-	public App()
+    public static WorktimeRepository WorktimeRepo { get; private set; }
+    public App(WorktimeRepository repo)
 	{
 		InitializeComponent();
 
 		MainPage = new AppShell();
+
+		WorktimeRepo = repo;
 	}
 }
---
+++ WorkTimer/Database/WorktimeRepository.cs
@@ -0,0 +1,61 @@
+﻿using SQLite;
+using WorkTimer.Models;
+
+namespace WorkTimer.Database
+{
+    public class WorktimeRepository
+    {
+        string _dbPath;
+
+        private SQLiteAsyncConnection conn;
+        
+        public string StatusMessage { get; set; }
+        
+        private async Task Init()
+        {
+            if (conn != null)
+                return;
+            
+            conn = new SQLiteAsyncConnection(_dbPath);
+            await conn.CreateTableAsync<WorktimeDay>();
+        }
+
+        public WorktimeRepository(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+
+        public async Task AddNewWorktimeDay(WorktimeDay worktime_day)
+        {
+            int result = 0;
+            try
+            {
+                await Init();
+
+                result = await conn.InsertAsync(worktime_day);
+
+                StatusMessage = string.Format("{0} record(s) added", result);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to add worktime. Error: {1}", ex.Message);
+            }
+
+        }
+
+        public async Task<List<WorktimeDay>> GetAllWorktime()
+        {
+            try
+            {
+                await Init();
+                return await conn.Table<WorktimeDay>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return new List<WorktimeDay>();
+        }
+    }
+}
