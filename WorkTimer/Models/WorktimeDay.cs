67aa6859e8b14c57c256f4a36c0ac6b0cc002748
DavidSchmidt00 <davod2211@gmail.com>
2023-08-02 22:10:09 +0200

    #4: WorktimeDay model

---
+++ WorkTimer/Models/WorktimeDay.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+
+namespace WorkTimer.Models
+{
+    [Table("Worktime")]
+    public class WorktimeDay
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+        
+        public TimeSpan WorkTime { get; set; }
+
+        public TimeSpan PauseTime { get; set; }
+
+        public bool Absent { get; set; }
+    }
+}
