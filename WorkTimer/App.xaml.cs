4893c19557cc0e41321dc378af3811f557506008
DavidSchmidt00 <davod2211@gmail.com>
2023-08-02 22:24:51 +0200

    #2: Custom window size

--- WorkTimer/App.xaml.cs
+++ WorkTimer/App.xaml.cs
@@ -13,4 +13,17 @@ public partial class App : Application
 
 		WorktimeRepo = repo;
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 800;
+        const int newHeight = 600;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
+    }
 }
