5d4bb978a48a799911f189900a0384ed130d0700
DavidSchmidt00 <davod2211@gmail.com>
2023-08-02 22:10:49 +0200

    #4: Load stored worktime into OverviewPage

--- WorkTimer/ViewModel/OverviewViewModel.cs
+++ WorkTimer/ViewModel/OverviewViewModel.cs
@@ -1,19 +1,40 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using WorkTimer.Models;
 
 namespace WorkTimer.ViewModel
 {
+    [QueryProperty("WorktimeList", "WorktimeList")]
     public partial class OverviewViewModel : ObservableObject
     {
+        [ObservableProperty]
+        ObservableCollection<WorktimeDay> worktimeList = new ObservableCollection<WorktimeDay>();
+
+        private readonly Task initTask;
+
+        public OverviewViewModel() 
+        {
+            this.initTask = LoadWorktime();
+        }
+
+
         [RelayCommand]
         async Task Back()
         {
             await Shell.Current.GoToAsync("..");
+        }
+
+        async private Task LoadWorktime()
+        {
+            List<WorktimeDay> result = await App.WorktimeRepo.GetAllWorktime();
+            Trace.WriteLine("Get finished: " + result.Count);
+            if (result.Count > 0)
+            {
+                WorktimeList.Clear();
+                foreach (WorktimeDay day in result) { WorktimeList.Add(day); }
+            }
         }
     }
 }
