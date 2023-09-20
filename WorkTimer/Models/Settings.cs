70987a6fc87eb5f7986ff55a5d1b00371aeb15f4
DavidSchmidt00 <davod2211@gmail.com>
2023-09-20 21:55:35 +0200

    #5: Add settings page

---
+++ WorkTimer/Models/Settings.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace WorkTimer.Models
+{
+    [Table("Settings")]
+    public class Settings
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public float WorktimeWeeklyHours { get; set; }
+
+        public int VacationDays { get; set; }
+
+        public bool StandingDesk { get; set; }
+    }
+}
--- WorkTimer/ViewModel/SettingsViewModel.cs
+++ WorkTimer/ViewModel/SettingsViewModel.cs
@@ -1,19 +1,55 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging;
+using System.Diagnostics;
+using WorkTimer.Models;
+using WorkTimer.Util;
+using WorkTimer.View;
 
 namespace WorkTimer.ViewModel
 {
     public partial class SettingsViewModel : ObservableObject
     {
+        [ObservableProperty]
+        public float settingsWorktimeWeeklyHours;
+
+        [ObservableProperty]
+        public int settingsVacationDays;
+
+        [ObservableProperty]
+        public bool settingsStandingDesk;
+
         [RelayCommand]
         async Task Back()
         {
             await Shell.Current.GoToAsync("..");
+        }
+
+        [RelayCommand]
+        async Task Save()
+        {
+            Settings newSettings = new() { Id = 1, WorktimeWeeklyHours = SettingsWorktimeWeeklyHours, VacationDays = SettingsVacationDays, StandingDesk = SettingsStandingDesk };
+            await App.WorktimeRepo.UpdateSettings(newSettings);
+            string statusMessage = App.WorktimeRepo.StatusMessage;
+            Trace.WriteLine(statusMessage);
+        }
+
+        private readonly Task initTask;
+        public SettingsViewModel()
+        {
+            this.initTask = LoadWorktime();
+        }
+
+        async private Task LoadWorktime()
+        {
+            Settings settings = await App.WorktimeRepo.GetSettings();
+            Trace.WriteLine("Got settings");
+            if (settings is not null)
+            {
+                SettingsWorktimeWeeklyHours = settings.WorktimeWeeklyHours;
+                SettingsVacationDays = settings.VacationDays;
+                SettingsStandingDesk = settings.StandingDesk;
+            }
         }
     }
 }
